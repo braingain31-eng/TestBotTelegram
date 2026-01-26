9423dfe25ab4a7ad6ba59acdc82f5b894bbc0daa
braingain31 <braingain31@gmail.com>
2026-01-26 04:09:35 +0000

    sfgc

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM python:3.10-slim
 
-WORKDIR /app
+ENV PYTHONUNBUFFERED True
 
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+
+# Install dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "--bind", "0.0.0.0:$PORT", "--worker-class", "sync", "main:app"]
+ENV GUNICORN_CMD_ARGS "--workers=2 --threads=4 --timeout=120 --keep-alive=75"
+
+CMD ["sh", "-c", "gunicorn $GUNICORN_CMD_ARGS -b 0.0.0.0:8080 main:app"]
--- config.py
+++ config.py
@@ -8,6 +8,15 @@ class Config:
     ADMIN_ID = int(os.getenv('ADMIN_ID'))
     ADMIN_CHANNEL_ID = int(os.getenv('ADMIN_CHANNEL_ID'))
     LOCATION_NAME = os.getenv('LOCATION_NAME', 'Северное Гоа')
-    FIREBASE_CREDENTIALS = os.getenv('FIREBASE_CREDENTIALS')  # путь к JSON или содержимое
+    # FIREBASE_CREDENTIALS = os.getenv('FIREBASE_CREDENTIALS')  # путь к JSON или содержимое
+    FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH", "/etc/secrets/firebase.json")
     WEBHOOK_URL = os.getenv('WEBHOOK_URL')
     PORT = int(os.getenv('PORT', 8080))
+
+
+
+
+
+
+
+# FIREBASE_CREDENTIALS_PATH = os.getenv("FIREBASE_CREDENTIALS_PATH", "/etc/secrets/firebase.json")
